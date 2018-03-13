9ad83af3d8638a15ff4cb074c49cddf4692e2516
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-03-13 18:36:36 +0000

    Added particles.js for the 96boards.ai background

--- assets/js/package-home-ai.js
+++ assets/js/package-home-ai.js
@@ -1,12 +1,13 @@
 //= require plugins/jquery1.11.2.min
 //= require plugins/bootstrap.min
 //= require plugins/scrollreveal.min
-//= require plugins/starfield.min
 //= require vendor/cookieconsent.min
 //= require vendor/lazysizes.min
 //= require vendor/ls.unveilhooks
 //= require app/jquery.mobile.custom.min
 
 //= require app/main
+//= require app/particles
+//= require app/home
 //= require app/custom
 
