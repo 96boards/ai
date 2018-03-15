4931c092c32d14b5d62a9f4771e6668972e0d6bc
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-03-15 02:50:47 +0000

    Added the lightbox to the products JS package

--- assets/js/package-products.js
+++ assets/js/package-products.js
@@ -1,15 +1,12 @@
 //= require plugins/jquery1.11.2.min
 //= require plugins/bootstrap.min
-//= require plugins/scrollreveal.min
 //= require vendor/cookieconsent.min
 //= require vendor/lazysizes.min
 //= require vendor/ls.unveilhooks
-//= require vendor/mc-validate
-//= require vendor/owl.carousel.min
 //= require app/jquery.mobile.custom.min
+//= require vendor/lightbox
 
 //= require app/main
 //= require app/particles
-//= require app/home
 //= require app/custom
 
