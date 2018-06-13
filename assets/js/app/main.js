373331e5fc64063ea520e58feb3129043d6d404e
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-06-13 15:55:15 +0100

    Updated the anchor JS for opening external links in new windows

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -116,8 +116,10 @@ $(document).ready(function () {
     // External Links Opening in new window.
     $('a').each(function() {
        var a = new RegExp('/' + window.location.host + '/');
-       if($(this).attr("target") != "_self"){
-           $(this).attr("target","_blank");
+       if (!a.test(this.href)) {
+           if($(this).attr("target") != "_self"){
+               $(this).attr("target","_blank");
+           }
        }
     });
     
