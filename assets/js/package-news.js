454c1361ecb0717a75eafcac12e3312a1f114777
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-04-26 17:09:39 +0100

    Added a news JS package

---
+++ assets/js/package-news.js
@@ -0,0 +1,9 @@
+//= require vendor/jquery
+//= require vendor/bootstrap
+//= require vendor/cookieconsent
+//= require vendor/lazysizes
+//= require vendor/ls.unveilhooks
+
+//= require app/main
+//= require app/news
+//= require app/custom
