8ac4e08ed895c0e557e83d9a07ff4b3e9bbb4e85
Philip Colmer <philip.colmer@linaro.org>
2019-07-04 12:02:38 +0100

    Remove -i from docker run

--- build-site.sh
+++ build-site.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 if [ -z "$JEKYLLSITEBUILD" ]; then
-        export JEKYLLSITEBUILD=latest
+  export JEKYLLSITEBUILD=latest
 fi
-
 if [ -z "$JEKYLL_ENV" ]; then
 	export JEKYLL_ENV=staging
 fi
 docker run \
   --cap-drop ALL \
   --rm \
-  -it \
+  -t \
   -p 4000:4000 \
   -e JEKYLL_ACTION \
   -e JEKYLL_CONFIG \
