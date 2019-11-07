b4fe99cb6bad4f7d6cddbed53dda283f6dd2abd2
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-11-07 10:31:14 +0000

    Removed old JS packages

--- assets/js/package-news.js
+++
@@ -1,9 +0,0 @@
-//= require vendor/jquery
-//= require vendor/bootstrap
-//= require vendor/cookieconsent
-//= require vendor/lazysizes
-//= require vendor/ls.unveilhooks
-
-//= require app/main
-//= require app/news
-//= require app/custom
