4043522e2685fba648248e83aa20b726d6d697c8
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-11-07 10:30:37 +0000

    Added new package to be used on both pages pulling the AI boards from 96boards.org/assets/json/ai_boards.json

---
+++ assets/js/package-base.js
@@ -0,0 +1,10 @@
+//= require vendor/jquery
+//= require vendor/bootstrap
+//= require vendor/cookieconsent
+//= require vendor/lazysizes
+//= require vendor/owl.carousel
+
+//= require app/main
+//= require app/ai_boards
+//= require app/particles
+//= require app/custom
